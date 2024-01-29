d2679437c91c99c91f5a646c57b0b95a5e86a121
sijo_joshua <sijojoshua@duck.com>
2024-01-29 12:37:48 +0530

    menu icon fix

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -37,7 +37,7 @@ const NavBar = () => {
         </div>
         <div className="list-wrapper">
           <img
-            src="https://private-user-images.githubusercontent.com/113039157/299427061-c4ec12ca-c009-4883-89a6-e4ce1f189d92.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDYxMjI5OTksIm5iZiI6MTcwNjEyMjY5OSwicGF0aCI6Ii8xMTMwMzkxNTcvMjk5NDI3MDYxLWM0ZWMxMmNhLWMwMDktNDg4My04OWE2LWU0Y2UxZjE4OWQ5Mi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTI0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEyNFQxODU4MTlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT03OWI4NzFlMDg1NDY5YjExZWNkMDk3MTlkNmFiYjgwZGYwZTQ2NjUwMDU4MzQ5OGJiYzg2ZWI2MDYyNGI4NDAyJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.3j5Ui01xLL5Mgg_AfW3STgbZ_yingWzjT_MfedIxmPc"
+            src="https://toppng.com/uploads/preview/menu-icon-png-3-lines-11552739310fjzs2n2wxt.png"
             alt="Menu bars"
             style={{ opacity: !open ? 1 : 0 }}
             onClick={() => {
@@ -45,7 +45,7 @@ const NavBar = () => {
             }}
           />
           <img
-            src="https://private-user-images.githubusercontent.com/113039157/299427572-a14e2d07-3021-4703-994a-1142863845af.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDYxMjMxNDQsIm5iZiI6MTcwNjEyMjg0NCwicGF0aCI6Ii8xMTMwMzkxNTcvMjk5NDI3NTcyLWExNGUyZDA3LTMwMjEtNDcwMy05OTRhLTExNDI4NjM4NDVhZi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTI0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEyNFQxOTAwNDRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00NTViZjM1ZTk0M2M4YzBkMzY3ZWMxMDE0YjcwZTQ4NzZhOGE3MjVjOWVkNjE1MmM1ODQ5MjgzMzEwZjcwYzM4JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.Or336_qUSJCLybp2i0_fe5-yopUWweCxl3kQfB1vuv8"
+            src="https://www.dlf.pt/dfpng/middlepng/131-1318712_png-file-svg-mobile-menu-close-icon-transparent.png"
             alt="Menu cross"
             style={{ opacity: open ? 1 : 0 }}
             onClick={() => {
